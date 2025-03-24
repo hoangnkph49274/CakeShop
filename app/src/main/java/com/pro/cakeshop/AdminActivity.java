e24e47219365f1e9750fcf73cb76834c4bc7df14
hieundph49159 <162280614+hieundph49159@users.noreply.github.com>
2025-03-24 13:26:37 +0700

    Đức Hiếu - push login

---
+++ app/src/main/java/com/pro/cakeshop/AdminActivity.java
@@ -0,0 +1,13 @@
+package com.pro.cakeshop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
